994b3f31db3f07ef3a3a7344afb2ff4a25387532
elgatonista <siliconchik@gmail.com>
2016-11-05 18:34:11 +0300

    finished main task to show dad's name, but slacking with a CRUD stuff

--- 05/whos_your_daddy.py
+++ 05/whos_your_daddy.py
@@ -16,22 +16,36 @@
 # should prints out the grandfathers name. You should use same dict.
 ###
 
-print("Please input the name so the program will produce the name of his father.\n")
-
+print("Please input the name so the program will produce the name of his father.")
+print("--")
 names_dad = {
-    'Serg': 'Mikhail',
-    'Misha': 'Serg',
-    'Anya': 'Serg',
-    'Senya': 'Misha',
-    'Sasha': 'Andrey',
-    'Anatoly': 'Valdimir',
-    'Irina': 'Anatoly',
-    'Polina': 'Anatoly',
-    'Aleksy': 'Sergey',
-    'Andrey': 'Sergey'
+    'Serg': ('Mikhail', 'Sirafim'),
+    'Misha': ('Serg', 'Mikhail'),
+    'Anya': ('Serg', 'Mikhail'),
+    'Senya': ('Misha', 'Serg'),
+    'Sasha': ('Andrey', 'Unknown'),
+    'Anatoly': ('Vladimir', 'Unknown'),
+    'Irina': ('Anatoly', 'Valdimir'),
+    'Polina': ('Anatoly', 'Vladimir'),
+    'Aleksy': ('Sergey', 'Vladimir'),
+    'Andrey': ('Sergey', 'Vladimir'),
+    'Natasha': ('Igor', 'Unknown')
 }
 
 for son in names_dad.keys():
     print(son)
 
 print("Write a name to found out who's father")
+get_name = input("Give me a name, please: ").title()
+if get_name in names_dad.keys():
+    print("{}'s father is {}.".format(get_name, names_dad[get_name][0]))
+    grand = input("Do you wanna know who was his grandfather? Y/n ")
+    if grand == 'y':
+        print("{}'s grandfather is {}.".format(get_name, names_dad[get_name][1]))
+    elif grand == 'n':
+        print("Bye, pal")
+else:
+    print("Sorry, I can't find {} in my database.".format(get_name))
+
+# a little bit slacking with doing a task, such as performing a loop
+# but I've already did it in rpg_character script.
